b163cdb9820dd1a25141e69b64736d0cdbc8f008
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-07-27 16:02:47 +0200

    Changed App.js to App.jsx(for linting purposes) and created ProfileForm component with state

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App(){
+    return(
+        <h1>Functional app</h1>
+    );
+}
+
+export default App;
---
+++ src/components/ProfileForm.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function ProfileForm(){
+
+    // User inputs
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [adress, setAdress] = useState();
+    const [description, setDescription] = useState();
+    const [image, setImage] = useState();
+
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default ProfileForm;
