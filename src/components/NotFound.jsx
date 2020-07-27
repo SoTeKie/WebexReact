f1dc65e8a73c58ba519eadff14901e27df3db307
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-07-27 22:56:37 +0200

    Final styling for 404 component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from '../Stylesheets/Center.module.css'
 
 function NotFound(){
     return(
-        <div>
+        <div className={style.center}>
             <h1>The page you were looking for was not found</h1>
-            <Link to='/view-all'>Return to homepage</Link>
+			<Link to='/view-all'><p>Return to homepage</p></Link>
         </div>
     );
 }
